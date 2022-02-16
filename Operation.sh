9446f27890873af2c158c79fae9ed04bc2888fc2
pappurathod <prathod4545@gmail.com>
2022-02-16 20:54:33 +0530

    Added substraction features

--- Operation.sh
+++ Operation.sh
@@ -3,9 +3,16 @@ function addition () {
 	echo "the addition is $cal"
 }
 
+function substraction () {
+        cal=$(($a-$b))
+        echo "the substraction is $cal"
+}
+
+
 echo "enter first number"
 read a
 echo "enter second number"
 read b
 
 addition
+substraction
